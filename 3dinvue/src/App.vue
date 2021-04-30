d594b7710bc05327cceff5e65a7e7654839a3d84
aakash47 <aakash91762@gmail.com>
2021-04-30 12:00:47 +0530

    componets added

---
+++ 3dinvue/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Eddy />
+  </div>
+</template>
+
+<script>
+import Eddy from './components/Eddy.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Eddy
+  }
+}
+</script>
+
+<style>
+html,
+body,
+#app {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
